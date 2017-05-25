5eb4abc432d4c90ba3aabe31fdd5fea98e65c386
xingwus <wangyu19930107@qq.com>
2017-05-25 17:38:32 +0800

    update

--- src/main/java/cn/reawei/api/ApiReaweiApplication.java
+++ src/main/java/cn/reawei/api/ApiReaweiApplication.java
@@ -1,10 +1,22 @@
 package cn.reawei.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiReaweiApplication {
+public class ApiReaweiApplication extends SpringBootServletInitializer {
+
+
+	private static final Logger logger = LoggerFactory.getLogger(ApiReaweiApplication.class);
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiReaweiApplication.class, args);
